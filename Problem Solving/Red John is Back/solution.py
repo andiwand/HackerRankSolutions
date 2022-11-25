4067175e55ebf6ee223e66c4c1d046e3bd0f5a0a
andiwand <stefl.andreas@gmail.com>
2022-11-25 14:37:36 +0100

    missing changes

--- Problem Solving/Red John is Back/solution.py
+++ Problem Solving/Red John is Back/solution.py
@@ -18,18 +18,14 @@ def redJohn(n):
         elements = [{1:n-4*h, 4:h} for h in range(n//4+1)]
         return sum(math.factorial(e[1]+e[4])//(math.factorial(e[1])*math.factorial(e[4])) for e in elements)
     def primes(n):
-        p = [True]*n
-        p[0] = p[1] = False
-        for i in range(math.floor(math.sqrt(n))):
-            print(i)
+        p = [False,False]+[True]*(n-1)
+        for i in range(n):
             if not p[i]:
                 continue
-            for j in range(i*2, n, i):
+            for j in range(i*2,n+1,i):
                 p[j] = False
         return sum(1 if i else 0 for i in p)
-    w = walls(n)
-    p = primes(w)
-    return p
+    return primes(walls(n))
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
